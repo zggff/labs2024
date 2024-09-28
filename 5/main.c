193bdf0a4c1fb399947c58ffb4e968459ba80c7f
zggff <gigamaximwachau@gmail.com>
2024-09-28 13:31:01 +0300

    lab5 fix bug

--- 5/main.c
+++ 5/main.c
@@ -36,7 +36,7 @@ int factorial2(int n, double *res) {
         return MATH_OKAY;
     }
     *res = 1;
-    for (int i = n % 2 + 2; i <= n; i++) {
+    for (int i = n % 2 + 2; i <= n; i+=2) {
         *res *= i;
         if (isinf(*res)) {
             return MATH_OVERFLOW;
