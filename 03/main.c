ad64b2f6e3ddbce50a12a7dc88df33a1edcdbbbd
zggff <gigamaximwachau@gmail.com>
2024-11-06 20:48:05 +0300

    read file + fix makefile

--- 03/main.c
+++ 03/main.c
@@ -1,9 +1,11 @@
 #include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "lib.h"
+#include "tokenize.h"
 
 int main(int argc, char *argw[]) {
     if (argc < 2) {
@@ -18,13 +20,20 @@ int main(int argc, char *argw[]) {
         return 1;
     }
 
-    Polynom p = {0};
-    int r =
-        polynom_parse_str(&p, "12 * xy^12 * x21^4 * -2 * xy^-3 + 12 *z -1 - x");
-    printf("r = %d\n", r);
-    polynom_print(&p);
-    polynom_free(&p);
+    char **toks = NULL;
+    size_t tok_cap = 0;
+    char buf[BUF_SIZE] = {0};
+    int off = 0;
+    while (true) {
+        int n = token_parse_file(&toks, &tok_cap, buf, &off, f);
+        if (n <= 0)
+            break;
 
+        token_print(stdout, toks);
+        for (int i = 0; i < n; i++)
+            free(toks[i]);
+    }
+    free(toks);
     fclose(f);
     return 0;
 }
